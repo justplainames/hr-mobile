eac103d8eef3ed5c42784994ea5e4e448af076d6
justplainames <justplainames@hotmail.com>
2022-03-14 17:30:05 +0800

    Added biometrics

--- app/src/main/java/edu/singaporetech/hr/ForgetUserFragment.kt
+++ app/src/main/java/edu/singaporetech/hr/ForgetUserFragment.kt
@@ -1,12 +1,19 @@
 package edu.singaporetech.hr
 
+import android.content.ContentValues
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import edu.singaporetech.hr.databinding.FragmentForgetUserBinding
 
 class ForgetUserFragment : Fragment() {
@@ -20,9 +27,41 @@ class ForgetUserFragment : Fragment() {
             R.layout.fragment_forget_user, container, false
         )
         binding.btnSubmit.setOnClickListener { view: View ->
-            view.findNavController()
-                .navigate(R.id.action_forgetUserFragment_to_recoverSuccessFragment)
+            recover(binding.etSecondaryEmail.text.toString())
         }
         return binding.root
+    }
+
+    private fun recover(email: String) {
+        val recoveryEmail = view?.findViewById<EditText>(R.id.etSecondaryEmail)
+        if (!validateForm()) {
+            return
+        }else{
+            FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                .addOnCompleteListener{task ->
+                    if(task.isSuccessful){
+                        view?.findNavController()
+                            ?.navigate(R.id.action_forgetUserFragment_to_recoverSuccessFragment)
+                    } else {
+                        recoveryEmail?.error = "Email not in database."
+                    }
+                }
+        }
+
+
+    }
+
+    private fun validateForm(): Boolean {
+        val email = view?.findViewById<EditText>(R.id.etSecondaryEmail)
+        var valid = true
+        email?.text.toString()
+        if (TextUtils.isEmpty(email?.text)) {
+            email?.error = "Required."
+            valid = false
+        } else {
+
+        }
+
+        return valid
     }
 }
--- app/src/main/java/edu/singaporetech/hr/SignUserFragment.kt
+++ app/src/main/java/edu/singaporetech/hr/SignUserFragment.kt
@@ -1,56 +1,77 @@
 package edu.singaporetech.hr
 
 import android.app.Activity
+import android.app.KeyguardManager
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
 import android.os.Bundle
+import android.os.CancellationSignal
 import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import edu.singaporetech.hr.databinding.FragmentSignUserBinding
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 
 class  SignUserFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
+    private var cancellationSignal: CancellationSignal? = null
+    private val authenticationCallback: BiometricPrompt.AuthenticationCallback
+        get() =
+            @RequiresApi(Build.VERSION_CODES.P)
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                    super.onAuthenticationError(errorCode, errString)
+                    notifyUser("Authentication error: $errString")
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                    super.onAuthenticationSucceeded(result)
+                    notifyUser("Authentication success!")
+                    startActivity(Intent(activity, MainActivity::class.java))
+                }
+            }
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val user = Firebase.auth.currentUser
-        if (user != null) {
-            val intent = Intent(activity, MainActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
-        }
-
         val binding = DataBindingUtil.inflate<FragmentSignUserBinding>(
             inflater,
             R.layout.fragment_sign_user, container, false
         )
 
+        val user = Firebase.auth.currentUser
+
+        if (user != null) {
+            binding.tvFaceId.visibility = View.VISIBLE
+            binding.ibFaceId.visibility = View.VISIBLE
+            binding.imageView.visibility = View.VISIBLE
+            biometricAuthentication()
+        } else{
+            binding.tvFaceId.visibility = View.INVISIBLE
+            binding.ibFaceId.visibility = View.INVISIBLE
+            binding.imageView.visibility = View.INVISIBLE
+        }
+
         auth = FirebaseAuth.getInstance()
 
-        val currentUser = auth.currentUser
-        if(currentUser != null){
-
-        }
 
         binding.btnLogin.setOnClickListener { view: View ->
             hideKeyboard()
@@ -67,9 +88,12 @@ class  SignUserFragment : Fragment() {
             view.findNavController().navigate(R.id.action_signUserFragment_to_signFacialFragment)
         }
 
+
+        binding.ibFaceId.setOnClickListener {
+            biometricAuthentication()
+        }
+
         return binding.root
-
-
     }
 
     private fun signIn(email: String, password: String) {
@@ -128,8 +152,73 @@ class  SignUserFragment : Fragment() {
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
+    private fun notifyUser(message: String) {
+        Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+    }
 
+    private fun getCancellationSignal() : CancellationSignal {
+        cancellationSignal = CancellationSignal()
+        cancellationSignal?.setOnCancelListener {
+            notifyUser("Authentication was cancelled by the user")
+        }
+        return cancellationSignal as CancellationSignal
+    }
 
+    private fun checkBiometricSupport(): Boolean {
+        val keyguardManager = requireActivity().getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
 
+        if (!keyguardManager.isKeyguardSecure){
+            notifyUser("Fingerprint authentication has not been enabled in settings")
+            return false
+        }
+
+        if (activity?.let { ActivityCompat.checkSelfPermission(it.applicationContext, android.Manifest.permission.USE_BIOMETRIC) } != PackageManager.PERMISSION_GRANTED) {
+            notifyUser("Fingerprint authentication permission is not enabled")
+            return false
+        }
+
+        return if (requireContext().packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)){
+            true
+        } else true
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun biometricAuthentication(){
+        checkBiometricSupport()
+        val biometricPrompt = activity?.let { it1 ->
+            BiometricPrompt.Builder(activity)
+                .setTitle("You were not fully logged out!")
+                .setSubtitle("Sign in quikcly using your biometric credentials")
+                .setNegativeButton(
+                    "Cancel",
+                    it1.mainExecutor,
+                    DialogInterface.OnClickListener { dialog, which ->
+                        notifyUser("Authentication Cancelled")
+                    }).build()
+        }
+
+        if (biometricPrompt != null) {
+            biometricPrompt.authenticate(
+                getCancellationSignal(),
+                requireContext().mainExecutor,
+                authenticationCallback
+            )
+        }
+    }
+
+    private fun updateUI(input: Int) {
+        var faceid = view?.findViewById<TextView>(R.id.tvFaceId)
+        var faceimg = view?.findViewById<ImageButton>(R.id.ib_faceId)
+        var imgView = view?.findViewById<ImageView>(R.id.imageView)
+        if(input == 1){
+            faceid?.visibility = View.VISIBLE
+            faceimg?.visibility = View.VISIBLE
+            imgView?.visibility = View.VISIBLE
+        } else {
+            faceid?.visibility = View.INVISIBLE
+            faceimg?.visibility = View.INVISIBLE
+            imgView?.visibility = View.INVISIBLE
+        }
+    }
 }
 
